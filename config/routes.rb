9a729eaa64a9582c9167a7c2581d49573a0a4f10
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-04-28 07:36:19 +0200

    Add controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: 'json' } do
+    get 'greeting', to: 'greetings#index'
+  end
+
+  get '*page', to: 'static#index', constraints: lambda { |req|
+    !req.xhr? && req.format.html?
+  }
+
+  root 'static#index'
 end
